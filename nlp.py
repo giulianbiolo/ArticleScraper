a3fbd57c165c701f5bbb4bad10430001db63d02a
giulianbiolo <giulianbiolo@gmail.com>
2022-03-17 22:36:22 +0100

    Rinnovato algoritmo di ricerca

--- nlp.py
+++ nlp.py
@@ -4,18 +4,21 @@ import Levenshtein
 
 def get_affine_feeds(query_title: str, feeds: list[Feed]) -> list[Feed]:
     '''Questo metodo ritorna i feed attinenti al testo ricercato, osservando la vicinanza al titolo.'''
-    query_title = query_title.lower()
-    query_title = prettify_text(query_title)
-    affine_feeds: list[Feed] = []
-    for feed in feeds:
-        if Levenshtein.ratio(query_title, prettify_text(feed.title.lower())) > 0.8:
-            affine_feeds.append(feed)
-            continue
-        for word in prettify_text(feed.title.lower()).split():
-            if Levenshtein.ratio(query_title, word) > 0.8:
-                affine_feeds.append(feed)
-                break
-    return affine_feeds
+    query_title = prettify_text(query_title.lower())
+    feeds.sort(key=lambda feed: calc_highest_token_score(query_title, prettify_text(feed.title.lower())), reverse=True)
+    return feeds
+
+
+def calc_highest_token_score(query_title: str, feed_title: str) -> float:
+    '''Questo metodo calcola il miglior score di affinità tra i token del titolo.'''
+    tokens_query_title = query_title.split(" ")
+    tokens_feed_title = feed_title.split(" ")
+    token_score_overall: float = 0.0
+    for token_query_title in tokens_query_title:
+        for token_feed_title in tokens_feed_title:
+            token_score_overall += Levenshtein.ratio(token_query_title, token_feed_title)
+    return token_score_overall
+
 
 
 def prettify_text(text: str) -> str:
